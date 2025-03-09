8c189eae8aae2a695a2640ac7c13b02992c54863
Adithya K B <adithyakb93@gmail.com>
2025-03-09 10:43:15 +0530

    feat: Create status/stories bar

---
+++ components/StatusBar.tsx
@@ -0,0 +1,47 @@
+import { ThemedView } from "./ThemedView";
+import { ThemedText } from "./ThemedText";
+import { Image } from "react-native";
+
+const artists = [
+  { name: "Arijit", image: require("@/assets/images/artists/arjit.png") },
+  { name: "Conan", image: require("@/assets/images/artists/conan.png") },
+  { name: "Weeknd", image: require("@/assets/images/artists/weeknd.png") },
+  { name: "Younger", image: require("@/assets/images/artists/younger.png") },
+];
+
+export default function StatusBar() {
+  return (
+    <ThemedView
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        gap: 10,
+        paddingVertical: 5,
+        paddingHorizontal: 10,
+      }}
+    >
+      {artists.map((artist, index) => (
+        <ThemedView
+          key={index}
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Image
+            source={artist.image}
+            style={{
+              width: 60,
+              height: 60,
+              borderRadius: 100,
+              borderWidth: 3,
+              borderColor: "gray",
+            }}
+          />
+          <ThemedText style={{ fontSize: 13 }}>{artist.name}</ThemedText>
+        </ThemedView>
+      ))}
+    </ThemedView>
+  );
+}
